5902773c00db684828554718200247968ac1794a
ILOMI.LIMOI <chahabdarhossein@gmail.com>
2023-07-25 11:43:42 +0330

    posts model configuration
    added Posts to the models and registered at admin ready for superusers usage

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from blog.models import Posts
 # Register your models here.
+
+
+class PostsAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('title', 'author', 'status', 'created_at', 'updated_at')
+    empty_value_display = "EMPTY"
+    list_filter = ('status', 'author')
+    search_fields = ('titles',)
+
+admin.site.register(Posts,PostsAdmin)
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Posts(models.Model):
+    image = models.ImageField(upload_to="blog", default="/blog/default.jpg")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) 
+    title = models.CharField(max_length=30)
+    content = models.TextField(max_length=500)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        ordering = ['created_at']
+
+
+    def __ster__(self):
+        return self.title
